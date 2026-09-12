d6e866e77c61c88d027d5362686b18ad8d7d9fd3
quan.pham <quanpm.mta@gmail.com>
2026-09-12 14:28:50 +0700

    Bump patchdesk to 0.0.8

--- Casks/patchdesk.rb
+++ Casks/patchdesk.rb
@@ -1,6 +1,6 @@
 cask "patchdesk" do
-  version "0.0.7"
-  sha256 "c24bce4c886b69c9c8924b312065d83ac6888782efc8ed1b05e4752c8feb81a9"
+  version "0.0.8"
+  sha256 "cb7cef0299b9f3782a624c9fcf7af6a254bc9d4684071ae8fd38ec698f2eb02a"
 
   url "https://github.com/kwanpham2195/patchdesk/releases/download/v#{version}/Patchdesk-#{version}-arm64.dmg"
   name "Patchdesk"
